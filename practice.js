702f50952e9430ddb12b5417184a19a9cad2d16c
SANKET45IAS <sanket45nayak@gmail.com>
2025-09-04 20:12:06 +0530

    middleware next

--- practice.js
+++ practice.js
@@ -7,3 +7,31 @@ function schema(obj) {
 }
 
 schema({ name: "Sanket", age: 20 }); 
+
+
+//Middleware 'next()'
+function out() {
+  console.log("Out");
+}
+
+// middleware-style function
+function into(next) {
+  console.log("Into");  // do something before
+  next();               // call the next middleware
+  console.log("Back");  // do something after
+}
+
+// passing out as next
+into(out);
+
+
+"Anology inside the methon next() call internally next function code if call then it move req,res else return to where it call"
+// function next(){
+//   move to req,res resq();
+// }
+// next if click execute
+// resq(){
+//   return response
+// }
+// else
+// return where it call 
